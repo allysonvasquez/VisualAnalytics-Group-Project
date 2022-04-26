aa4eddaba2ebad7cfe9b861880776e4062196cec
avasque8 <avasque8@uncc.edu>
2022-04-25 21:52:55 -0400

    completed radio and sidebar

--- app_v2.py
+++ app_v2.py
@@ -11,138 +11,43 @@ from datetime import date
 #ref: https://gdp-indicators-app.herokuapp.com/
 #ref: https://itcs-4122-5122-prj-team-3.github.io/demo/vis/comparison
 
-''' ---- TODO ----
-Allyson
-- Create section for radio buttons that will go to our visualizations
-- Create visualization displaying dataframe
-https://www.kaggle.com/code/anastasiyabirina/full-eda-and-visualization
-    - ref
+# ---- TODO ----
+# ALLYSON
+#- Create section for radio buttons that will go to our visualizations
+#- Create visualization displaying dataframe
+#https://www.kaggle.com/code/anastasiyabirina/full-eda-and-visualization
+#    - ref
 
-Vena
-    - Create a home page that introduces our group & topic/data (can pull info from our proposal assignment)
-https://www.kaggle.com/code/ajaysuram/hotel-booking-prediction
-    - convert some charts from here
-
-
-Alex
-https://www.kaggle.com/code/mortrest/hotel-booking-dataset-2-4
-    - Bar chart of hotel types
-    - Pie chart showing arrival_date_year (what year was most popular to get booked)
-    - Any feature on the x-axis. Y-axis = adr 
-        (adr = avg. daily rate. It is how much $ a customer paid for their hotel stay)
-
-Mandev
-https://www.kaggle.com/code/dangquangvu/hotel-booking-cleaning-data-bar-charts
-    - create visualization showing percentage of bookings per year (can use above links as reference)
-    - Convert final product into Heroku
-
-'''
+# VENA
+#    - Create a home page that introduces our group & topic/data (can pull info from our proposal assignment)
+#https://www.kaggle.com/code/ajaysuram/hotel-booking-prediction
+#    - convert some charts from here
 
 
-st.markdown("<h1 style='text-align: center; color: black;'>Hotel Booking Database</h1>", unsafe_allow_html=True)
+# ALEX
+#https://www.kaggle.com/code/mortrest/hotel-booking-dataset-2-4
+#    - Bar chart of hotel types
+#    - Pie chart showing arrival_date_year (what year was most popular to get booked)
+#    - Any feature on the x-axis. Y-axis = adr 
+#        (adr = avg. daily rate. It is how much $ a customer paid for their hotel stay)
 
-sample_source = pd.DataFrame({
-    'a': ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'],
-    'b': [28, 55, 43, 91, 81, 53, 19, 87, 52],
-    'c': [1,2,3,4,5,6,7,8,9]
-})
+# MANDEV
+#https://www.kaggle.com/code/dangquangvu/hotel-booking-cleaning-data-bar-charts
+#    - create visualization showing percentage of bookings per year (can use above links as reference)
+#    - Convert final product into Heroku
 
-sample_bar = alt.Chart(sample_source).mark_bar().encode(
-    x='a',
-    y='b'
-)
+@st.cache
+def load_data(): #Function that loads s ome data, puts it in a dataframe, and converts the date 
+    df = pd.read_csv('hotel.csv', index_col='idx')
+    return df
 
-sample_heatmap = alt.Chart(sample_source).mark_rect().encode(
-    x='a',
-    y='b',
-    color = 'c'
-)
+st.title('TITLE?')
 
-sample_line = alt.Chart(sample_source).mark_line().encode(
-    x='a',
-    y='b'
-)
+# load the data
+df = load_data()
 
-sample_scatter = alt.Chart(sample_source).mark_point().encode(
-    x='a',
-    y='b',
-    color = 'c'
-)
+viz = st.sidebar.radio("Select A Visualization Below", ('Show DataFrame', 'viz2','viz3','blah','test'))
 
-sample_stream = alt.Chart(sample_source).mark_area().encode(
-    x='a',
-    y='b',
-    color = 'c'
-)
-
-# Collapsable side bar with different view options
-with st.sidebar:
-    st.title('Customize Dashboard View')
-    with st.expander('Charts'):
-        chart_type = [st.checkbox('Chart 1'), st.checkbox('Chart 2'), st.checkbox('Chart 3'), st.checkbox('Chart 4'), st.checkbox('Chart 5')]
-
-    with st.expander('Hotel Type'):
-        hotel_type = [st.checkbox('City Hotel'), st.checkbox('Resort Hotel')]
-
-    with st.expander('Hotel Location'):
-        location = [st.checkbox('PRT'), st.checkbox('GBR'), st.checkbox('Other')]
-
-    with st.expander('Guest Attributes'):
-        numAdult = st.slider(
-            'Number of Adults',
-            min_value=0,
-            max_value=10,
-            value = 10
-        )
-        numChild = st.slider(
-            'Number of Chidren',
-            min_value=0,
-            max_value=10,
-            value = 10
-        )
-        numBaby = st.slider(
-            'Number of Babies',
-            min_value=0,
-            max_value=10,
-            value = 10
-        )
-    with st.expander('Reservation Attributes'):
-        st.slider(
-            'Select Number of Special Requests',
-            min_value = date(2015, 1, 1), 
-            max_value = date(2017, 12, 31),
-            value = (date(2015, 1, 1), date(2017, 12, 31))
-        )
-
-        st.slider(
-            'Select Booking Date:',
-            min_value = date(2015, 1, 1), 
-            max_value = date(2017, 12, 31),
-            value = (date(2015, 1, 1), date(2017, 12, 31))
-        )
-
-        st.slider(
-            'Select Arrival Date:',
-            min_value = date(2015, 1, 1), 
-            max_value = date(2017, 12, 31),
-            value = (date(2015, 1, 1), date(2017, 12, 31))
-        )
-
-        st.slider(
-            'Select Reservation Status Date:',
-            min_value = date(2015, 1, 1), 
-            max_value = date(2017, 12, 31),
-            value = (date(2015, 1, 1), date(2017, 12, 31))
-        )
-
-# Dashboard view
-if chart_type[0]:
-    st.write(sample_bar)
-if chart_type[1]:
-    st.write(sample_heatmap)
-if chart_type[2]:
-    st.write(sample_line)
-if chart_type[3]:
-    st.write(sample_scatter)
-if chart_type[4]:
-    st.write(sample_stream)
+# show the data in a table
+if viz == 'Show DataFrame':
+    st.write(df)
