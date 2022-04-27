c0edd62cd9604135669cebca41702e9a8af5c59b
Vena Khamvanthong <venakhamvan@gmail.com>
2022-04-27 19:27:02 -0400

    adding homepage

--- app_v2.py
+++ app_v2.py
@@ -41,12 +41,80 @@ def load_data(): #Function that loads s ome data, puts it in a dataframe, and co
     df = pd.read_csv('hotel.csv', index_col='idx')
     return df
 
-st.title('Hotel Booking Insights')
-
 # load the data
 df = load_data()
 
-viz = st.sidebar.radio("Select a Visualization Below to View", ('Statistical Overview', 'viz2','viz3','blah','test')) #rename as needed
+st.title('Hotel Booking Insights')
+with st.sidebar:
+    viz = st.radio("Select a Visualization Below to View", ('Home','Statistical Overview', 'viz2','viz3','blah','test'), 0) #rename as needed
+
+#vena-------
+if viz == 'Home':
+    st.write('ITCS 4122/5122 - Visual Analytics, Spring 2022')
+    st.write('Project Team 6: Allyson Vasquez, Alex Miller, Vena Khamvanthong, Mandev Doshi')
+
+    st.subheader('Project Overview')
+    st.write(
+        "For our visualization project, we chose to use the Kaggle dataset,[Hotel Booking](https://www.kaggle.com/datasets/mojtaba142/hotel-booking). "+
+        "This dataset contains over 110,000 entries representing hotel bookings made between July 1, 2015 and August 31, 2017. "+ 
+        "Our goals for this visualization are to determine that factors that influence booking cancellations and to find relationships between booking details, average daily rates, and final reservation status"
+    )
+
+    with st.expander("Dataset Attributes"):
+        st.write(
+            'The hotel booking dataset consisted of 36 attributes and 119,390 rows. For this project, we chose to focus on the ' +
+            'following key attributes:'
+        )
+        data_col1, data_col2, data_col3 = st.columns(3)
+        with data_col1:
+            """
+                Key Categorical Attributes
+                * adr (average daily rate)
+                * is_canceled
+                * is_repeated_guest
+                * customer_type
+                * distribution_channel
+            """
+
+        with data_col2:
+            """
+                Key Quanitative Attributes
+                * lead_time
+                * arrival_date
+                * previous_cancellations
+                * previous_bookings_not_canceled
+                * booking_changes
+                * total_special_requests
+                * reservation_status_date
+            """
+    with st.expander("Data Cleaning"):
+        st.write('The hotel booking dataset contained columns that were either artificially generated or not necessary for our ' + 
+                    'final visualizations and were dropped from the final dataframe. These columns include:')
+        """
+            * agent
+            * company
+            * name
+            * email
+            * phone-number
+            * credit_card
+            * arrival_date_month
+            * reservation_status_date
+            * reservation_status
+        """
+        st.write('In addition, there were a few instances of columns that have "na" or "Nan" values. These were the "children" and "country" columns.' +
+                    ' In the case of the children" column, since only four entries are affected, we dropped these entries altogether. The "country" column on the other hand had a significant number of affected entries. In this case, we changed all "na" values to "Unknown."'
+        )
+
+    with st.expander("Target Demographic"):
+        st.write('We designed our visualization to provide useful data concerning the trends that hotel bookings follow. The information would be most useful for indivduals in the hotel industry that would like to maximize profits '
+        )
+    with st.expander("Application Implementation"):
+        st.write('The web framework we used for our project was Streamlit, which allowed us the ease of creating a web ' +
+                    'without having to use HTML or CSS. For our visualizations, we created charts and graphs in Altair to ' +
+                    'allow for interactivity with the dataset. We also utilized the Pandas and Numpy in Python in order to '+
+                    'explore and clean the dataset.'
+        )
+#-------
 
 #allyson-------
 if viz == 'Statistical Overview':
@@ -57,6 +125,8 @@ if viz == 'Statistical Overview':
     option = st.selectbox('Select Your Hotel Type to Look at stats:', ('Resort Hotel', 'City Hotel'))
     st.write('You selected:', option)
 #-------
+
+
 
 if viz == 'viz2':
     #your viz here
